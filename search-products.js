fd7779be365b0e6c92407344f6bf902280c332d6
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-11 14:14:28 +0600

    bonus

---
+++ search-products.js
@@ -0,0 +1,26 @@
+const products = [
+    { name: 'samsung s3 phone', price: 12000 },
+    { name: 'asus laptop d34', price: 32000 },
+    { name: 'apple smart watch', price: 18000 },
+    { name: 'bosundhara binding paper', price: 80 },
+    { name: 'lg smart phone', price: 10000 },
+    { name: 'old granny land phone', price: 100 },
+    { name: 'samsung watch', price: 1000 },
+    { name: 'Dell laptop', price: 31000 },
+    { name: 'lenovo laptop', price: 41000 },
+]
+
+function searchProducts(products,searchText) {
+    let searchConvertToLower =searchText.toLowerCase();
+    const matched = [];
+    for (const product of products) {
+        const name = product.name;
+        if (name.indexOf(searchConvertToLower) != -1) {
+            matched.push(product);
+        }
+    }
+    return matched;
+}
+
+const matched = searchProducts(products, 'Laptop');
+console.log(matched);
